f5a6c04ed60fd7310aa1175488af7043d1de3841
Amanda Lowell <amandalowell@Amandas-MacBook-Pro.local>
2019-07-01 17:13:38 -0500

    product list

--- bamazon.sql
+++ bamazon.sql
@@ -12,3 +12,32 @@ CREATE TABLE products(
   PRIMARY KEY (item_id)	
 );
 
+
+insert into products (product_name, department_name, price, stock_quantity) 
+values ("socks", "clothes", 100, 40);
+
+insert into products (product_name, department_name, price, stock_quantity) 
+values ("mascara", "makeup", 12, 99);
+
+insert into products (product_name, department_name, price, stock_quantity) 
+values ("chocolate", "candy", 2, 20);
+
+insert into products (product_name, department_name, price, stock_quantity) 
+values ("watermelon", "fruit", 6, 88);
+
+insert into products (product_name, department_name, price, stock_quantity) 
+values ("pants", "clothes", 200, 66);
+
+insert into products (product_name, department_name, price, stock_quantity) 
+values ("eyeliner", "makeup", 22, 50);
+
+insert into products (product_name, department_name, price, stock_quantity) 
+values ("dresses", "clothes", 150, 5);
+
+insert into products (product_name, department_name, price, stock_quantity) 
+values ("strawberries", "fruit", 5, 35);
+
+insert into products (product_name, department_name, price, stock_quantity) 
+values ("skirts", "clothes", 20, 88);
+
+select * from products
