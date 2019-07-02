450afe1b25372511223d44abf2636f18cf5b760c
Amanda Lowell <amandalowell2016@gmail.com>
2019-07-02 09:38:53 -0500

    commentout

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,28 +15,6 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
-// display  all of the items available for sale. Include the ids, names, and prices of products for sale.
-
-
-
-
-//promp users with two messages :
-
-//1. ask them the ID of the product they would like to buy 
-//2. ask how many units of the product they would like to buy.
-
-
-
-//Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-
-//If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-
-
-
-
-//However, if your store does have enough of the product, you should fulfill the customer's order.
-//This means updating the SQL database to reflect the remaining quantity.
-//Once the update goes through, show the customer the total cost of their purchase.
 
 var mysql = require("mysql");
 var inquirer = require("inquirer");
@@ -61,7 +39,40 @@ connection.connect(function (err) {
    runSearch();
 });
 
-
+// ------------------------------- USER INPUT
+// function searchStore() {
+//     // prompt for info about the item being put up for auction
+//     inquirer
+//         .prompt([
+//             {
+//                 name: "product_id",
+//                 type: "input",
+//                 message: "What is the ID of the product that you would like to buy?"
+//             },
+//             {
+//                 name: "product_amount",
+//                 type: "input",
+//                 message: "How many units of that product would you like to buy?"
+//             },
+//             {
+//                 name: "startingBid",
+//                 type: "input",
+//                 message: "How many units of that product would you like to buy?",
+//                 validate: function (value) {
+//                     if (isNaN(value) === false) {
+//                         return true;
+//                     }
+//                     return false;
+//                 }
+//             }
+//         ])
+//         .then(function (answer) {
+//             // when finished prompting, insert a new item into the db with that info
+//             connection.query(
+//                 console.log("done")
+//             );
+//         });
+// }
 
 function productAmountSearch() {
    inquirer
@@ -100,54 +111,43 @@ function productAmountSearch() {
 
 productAmountSearch()
 
+// display  all of the items available for sale. Include the ids, names, and prices of products for sale.
 
-// connection.connect(function (err) {
-//     if (err) throw err;
-//     console.log("connected as id " + connection.threadId + "\n");
-//     readColleges();
-// });
 
-// function readColleges() {
-//     connection.query("SELECT name FROM colleges", function (err, res) {
-//         if (err) throw err;
 
-//         // Log all results of the SELECT statement
+
+//promp users with two messages :
+
+//1. ask them the ID of the product they would like to buy 
+//2. ask how many units of the product they would like to buy.
+
+
+
+//Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+
+//If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
+
+
+
+
+//However, if your store does have enough of the product, you should fulfill the customer's order.
+//This means updating the SQL database to reflect the remaining quantity.
+//Once the update goes through, show the customer the total cost of their purchase.
+
+ //connection.connect(function (err) {
+  //  if (err) throw err;
+  //   console.log("connected as id " + connection.threadId + "\n");
+  //   readColleges();
+ //});
+
+//function readColleges() {
+ //    connection.query("SELECT name FROM colleges", function (err, res) {
+//      if (err) throw err;
+
+         //Log all results of the SELECT statement
 //         console.log(res);
 //         connection.end();
 //     });
-// ------------------------------- USER INPUT
-// function searchStore() {
-//     // prompt for info about the item being put up for auction
-//     inquirer
-//         .prompt([
-//             {
-//                 name: "product_id",
-//                 type: "input",
-//                 message: "What is the ID of the product that you would like to buy?"
-//             },
-//             {
-//                 name: "product_amount",
-//                 type: "input",
-//                 message: "How many units of that product would you like to buy?"
-//             },
-//             {
-//                 name: "startingBid",
-//                 type: "input",
-//                 message: "How many units of that product would you like to buy?",
-//                 validate: function (value) {
-//                     if (isNaN(value) === false) {
-//                         return true;
-//                     }
-//                     return false;
-//                 }
-//             }
-//         ])
-//         .then(function (answer) {
-//             // when finished prompting, insert a new item into the db with that info
-//             connection.query(
-//                 console.log("done")
-//             );
-//         });
-// }
+
 
 // searchStore()
