47f5297457608dabb128eef336b18fe47ebb22ec
Amanda Lowell <amandalowell2016@gmail.com>
2019-07-02 09:30:41 -0500

    changes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -38,22 +38,22 @@ var connection = mysql.createConnection({
 //This means updating the SQL database to reflect the remaining quantity.
 //Once the update goes through, show the customer the total cost of their purchase.
 
-var mysql = require(“mysql”);
-var inquirer = require(“inquirer”);
+var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 // ------------------------------- CONNNECTION
 var connection = mysql.createConnection({
-   host: “localhost”,
+   host: "localhost",
 
    // Your port; if not 3306
    port: 3306,
 
    // Your username
-   user: “root”,
+   user: "root",
 
    // Your password
-   password: “rootroot”,
-   database: “bamazon”
+   password: "rootroot",
+   database: "bamazon"
 });
 
 connection.connect(function (err) {
@@ -68,19 +68,19 @@ connection.connect(function (err) {
 //     inquirer
 //         .prompt([
 //             {
-//                 name: “product_id”,
-//                 type: “input”,
-//                 message: “What is the ID of the product that you would like to buy?”
+//                 name: "product_id",
+//                 type: "input",
+//                 message: "What is the ID of the product that you would like to buy?"
 //             },
 //             {
-//                 name: “product_amount”,
-//                 type: “input”,
-//                 message: “How many units of that product would you like to buy?”
+//                 name: "product_amount",
+//                 type: "input",
+//                 message: "How many units of that product would you like to buy?"
 //             },
 //             {
-//                 name: “startingBid”,
-//                 type: “input”,
-//                 message: “How many units of that product would you like to buy?“,
+//                 name: "startingBid",
+//                 type: "input",
+//                 message: "How many units of that product would you like to buy?",
 //                 validate: function (value) {
 //                     if (isNaN(value) === false) {
 //                         return true;
@@ -92,7 +92,7 @@ connection.connect(function (err) {
 //         .then(function (answer) {
 //             // when finished prompting, insert a new item into the db with that info
 //             connection.query(
-//                 console.log(“done”)
+//                 console.log("done")
 //             );
 //         });
 // }
@@ -103,29 +103,29 @@ connection.connect(function (err) {
 function productAmountSearch() {
    inquirer
        .prompt({
-           name: “productID”,
-           type: “input”,
-           message: “What is the ID of the product that you would like to buy?”
+           name: "productID",
+           type: "input",
+           message: "What is the ID of the product that you would like to buy?"
        })
        .then(function (answer) {
-           var query = “SELECT item_id, stock_quantity”;
-           query += “FROM products ON (products.item_id = products.artist AND top_albums.year “;
-           query += “= top5000.year) WHERE (top_albums.artist = ? AND top5000.artist = ?) ORDER BY top_albums.year, top_albums.position”;
+           var query = "SELECT item_id, stock_quantity";
+           query += "FROM products ON (products.item_id = products.artist AND top_albums.year ";
+           query += "= top5000.year) WHERE (top_albums.artist = ? AND top5000.artist = ?) ORDER BY top_albums.year, top_albums.position";
 
            connection.query(query, [answer.artist, answer.artist], function (err, res) {
-               console.log(res.length + ” matches found!“);
+               console.log(res.length + " matches found!");
                for (var i = 0; i < res.length; i++) {
                    console.log(
-                       i + 1 + “.) ” +
-                       “Year: ” +
+                       i + 1 + ".) " +
+                       "Year: " +
                        res[i].year +
-                       ” Album Position: ” +
+                       " Album Position: " +
                        res[i].position +
-                       ” || Artist: ” +
+                       " || Artist: " +
                        res[i].artist +
-                       ” || Song: ” +
+                       " || Song: " +
                        res[i].song +
-                       ” || Album: ” +
+                       " || Album: " +
                        res[i].album
                    );
                }
@@ -140,12 +140,12 @@ productAmountSearch()
 
 // connection.connect(function (err) {
 //     if (err) throw err;
-//     console.log(“connected as id ” + connection.threadId + “\n”);
+//     console.log("connected as id " + connection.threadId + "\n");
 //     readColleges();
 // });
 
 // function readColleges() {
-//     connection.query(“SELECT name FROM colleges”, function (err, res) {
+//     connection.query("SELECT name FROM colleges", function (err, res) {
 //         if (err) throw err;
 
 //         // Log all results of the SELECT statement
